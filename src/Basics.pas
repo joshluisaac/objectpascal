12a76a1e7d3fdd0b8b35b1feb1ecf4fcf533934a
Joshua Nwankwo <jnwankwo@integradev.com.au>
2019-05-02 11:04:44 +1000

    Updated basics.pas

--- src/Basics.pas
+++ src/Basics.pas
@@ -1,28 +1,33 @@
 program Basics;
 
+{ syntax:
+ type
+ datatypeidentifier = typespecification;
+}
 type
   
   nType = Integer;
-  firstNameType = String;
-  middleNameType = String;
-  lastNameType = String;
-  ageType = Byte;
+  TfirstName = String;
+  TmiddleName = String;
+  TlastName = String;
+  Tage = Byte;
 
   //{$scopedenums on}
-  Tmonths = (Jan,Feb,Mar,April);
+  Tmonths = (Jan=1,Feb=2,Mar=3,April=7,Dec=12);
   //procedure printParamsProc(const A: Integer ; var B:Integer);
 
+//global constant
 const nConst : Integer = 900; 
 
+//global variable declaration
 var
   N : nType = 900;
   Y : Integer;
-
-  firstName : firstNameType;
-  MiddleName : middleNameType;
-  lastName : lastNameType;
-  age : ageType;
-  months : Tmonths;
+  firstName : TfirstName;
+  middleName : TmiddleName;
+  lastName : TlastName;
+  age : Tage;
+  birthMonth : Tmonths;
 
 
 {Params are semi-colon (;) separated and not comma(,) separated}
@@ -31,25 +36,49 @@ begin
     writeln('Printing integer: ', A, ',', B);
 end;
 
-procedure printFullNameProc(firstName: String ; MiddleName:String; lastName:System.String);
+{prints a given person's profile based on the args passed to the function}
+procedure printPersonProfile(firstName: TfirstName ; MiddleName:TmiddleName; lastName:System.String; birthMonth:Tmonths; age:Tage);
 begin
-  
-  writeln(firstName, ' ', MiddleName, ' ', lastName );
-  WriteLn('Birth month: ', months)
+  writeln(firstName, '|', MiddleName, '|', lastName, '|', birthMonth, '|', age );
+end;
+
+{ 
+  Prints the default person's profile.
+  The default person's profile is initialize using initSetValues.
+}
+procedure printPersonProfile();
+begin
+  writeln(firstName, '|', MiddleName, '|', lastName, '|', birthMonth, '|', age );
+end;
+
+// will initialize global vars
+procedure initSetValues;
+begin
+  Y := 100;
+  firstName := 'defaultFirstName';
+  middleName := 'defaultMiddleName';
+  lastName := 'defaultLastName';
+  age := 4;
+
+  //fetches the given item in enumerated type by value
+  birthMonth := Tmonths(7);
 end;
 
 
 
 begin
-    Y := 100;
-    firstName := 'Joshua';
+  initSetValues;
 
-    //fetches the 3rd item in enumerated type
-    months := Tmonths(2);
 
-    WriteLn(High(months));
-    printParamsProc(N,Y);
-    printFullNameProc(firstName,'Uzo','Nwankwo');
+  WriteLn(birthMonth);
+
+  WriteLn('Min month: ',Low(birthMonth));
+  WriteLn('Max month: ',High(birthMonth));
+  printParamsProc(N,Y);
+  printPersonProfile();
+  printPersonProfile(firstName,middleName,lastName,birthMonth,age);
+  printPersonProfile('Sam','Oku','Nwankwo',Tmonths(12),20);
+  printPersonProfile('Joshua','Uzo','Nwankwo',Tmonths(2),21);
 
 
 end.
