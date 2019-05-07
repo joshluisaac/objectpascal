5007a60f98fc965611deb9773aa184d227317f59
joshua <nwankwo.joshua@gmail.com>
2019-05-07 22:12:43 +1000

    Updated person app

--- main/personaddfrm.pas
+++ main/personaddfrm.pas
@@ -6,17 +6,17 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
-  PersonModel;
+  ActnList, StdActns, PersonModel;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    ActionList1: TActionList;
     saveBtn: TButton;
     cancelBtn: TButton;
     firstName: TEdit;
-    Edit2: TEdit;
     middleName: TEdit;
     lastName: TEdit;
     GroupBox1: TGroupBox;
@@ -24,10 +24,15 @@ type
     Label2: TLabel;
     Label3: TLabel;
     remarks: TMemo;
-    Panel1: TPanel;
+    SearchFind1: TSearchFind;
+    procedure cancelBtnClick(Sender: TObject);
     procedure firstNameChange(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure GroupBox1Click(Sender: TObject);
     procedure LabeledEdit1Change(Sender: TObject);
+    procedure Panel1Click(Sender: TObject);
     procedure saveBtnClick(Sender: TObject);
+
   private
 
   public
@@ -36,6 +41,7 @@ type
 
 var
   Form1: TForm1;
+  person : Tperson;
 
 implementation
 
@@ -48,14 +54,36 @@ begin
 
 end;
 
-procedure TForm1.saveBtnClick(Sender: TObject);
+procedure TForm1.Panel1Click(Sender: TObject);
 begin
 
+end;
+
+procedure TForm1.saveBtnClick(Sender: TObject);
+begin
+  remarks.Text:= '';
+  person := Tperson.create(firstName.Text,middleName.Text,lastName.text);
+  remarks.Lines.Add(person.format());
 end;
 
 procedure TForm1.firstNameChange(Sender: TObject);
 begin
 
+end;
+
+procedure TForm1.FormShow(Sender: TObject);
+begin
+    remarks.Text:= '';
+end;
+
+procedure TForm1.GroupBox1Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.cancelBtnClick(Sender: TObject);
+begin
+  Form1.Close;
 end;
 
 end.
